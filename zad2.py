362fc79b6da3bb75c2c4bd8488db8508890097c0
NataliaSkrodzka <nataliaskrodzka@gmail.com>
2024-06-07 08:38:51 +0200

    zad

--- zad2.py
+++ zad2.py
@@ -5,3 +5,20 @@ import pandas as pd
 xlsx = pd.ExcelFile('stacjepaliw.xlsx')
 df = pd.read_excel(xlsx, header=0)
 print(df)
+
+df1 = pd.melt(df,id_vars=['Nazwa'], var_name='Rok',value_name='Ilość')
+print(df1)
+dfs=df1[df1['Nazwa']=='ŚLĄSKIE']
+print(dfs)
+dfm=df1[df1['Nazwa']=='MAŁOPOLSKIE']
+print(dfm)
+
+plt.plot(dfs['Rok'],dfs['Ilość'], label='Śląskie')
+plt.plot(dfm['Rok'],dfm['Ilość'], label='Małopolskie')
+plt.xlabel('Lata')
+plt.ylabel('Ilość stacji')
+plt.title('Ilość stacji paliw w 2 województwach na przedziale lat')
+plt.legend()
+
+plt.savefig('stacjepaliw.png', format='png')
+plt.show()
